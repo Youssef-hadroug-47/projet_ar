f49a2f38f4d0bfbc39b2621b9c599386bd8b108c
Youssef-hadroug-47 <youssef.hadroug@insat.ucar.tn>
2026-04-30 00:07:27 +0100

    malek salut

--- src/main/java/com/job/authentication/Controller/AuthentificationController.java
+++ src/main/java/com/job/authentication/Controller/AuthentificationController.java
@@ -18,30 +18,30 @@ public class AuthentificationController {
         this.authService = authService;
     }
 
-    // 📝 POST /auth/register
+
     @PostMapping("/register")
     public String register(@RequestBody RegisterRequest request) {
         authService.register(request);
         return "User registered successfully";
     }
 
-    // 🔐 POST /auth/login
+
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
         return authService.login(request);
     }
 
-    // 🔄 POST /auth/refresh
+
     @PostMapping("/refresh")
     public AuthResponse refresh(@RequestParam String refreshToken) {
         return authService.refresh(refreshToken);
     }
 
-    // 🚪 POST /auth/logout
+
     @PostMapping("/logout")
     public String logout(@RequestHeader("Authorization") String header) {
 
-        String token = header.substring(7); // remove "Bearer "
+        String token = header.substring(7); 
         authService.logout(token);
 
         return "Logged out successfully";
