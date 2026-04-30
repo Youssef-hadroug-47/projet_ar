7760005b50baab166fbb7974f0dd6daca29fb27b
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-04-30 00:18:07 +0000

    src/main/java/com/job/models/SeekerSkill.java

---
+++ src/main/java/com/job/seekers/respository/SkillRepository.java
@@ -0,0 +1,12 @@
+package com.job.seekers.respository;
+
+import com.job.models.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+    Optional<Skill> findByName(String name);
+}
