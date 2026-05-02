4d9e3ff825226d196992294ce4f032fc8294004b
Youssef-hadroug-47 <youssef.hadroug@insat.ucar.tn>
2026-05-02 20:29:47 +0100

    a7m

--- claude/src/main/java/com/job/common/exception/TokenException.java
+++
@@ -1,9 +0,0 @@
-package com.job.common.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class TokenException extends RuntimeException {
-    public TokenException(String message) { super(message); }
-}
